a09aeabc18fe3f93044d74d089e10bfa2f3ad6be
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-20 11:32:44 +0530

    new things

--- B_Large_Addition.cpp
+++ B_Large_Addition.cpp
@@ -15,16 +15,44 @@ int main()
 
     ll t, n;
     cin >> t;
+    int total_digits = 0;
+    int flag_compl = 0;
     for (int i = 0; i < t; i++)
     {
         cin >> n;
-        for (int i = 0; i < (int)(ceil(log10(n))); i++)
+        flag_compl=0;
+        total_digits = (int)(ceil(log10(n)));
+        for (int i = 0; i < total_digits; i++)
         {
-            if (i == 0)
+            if (i == 0 && return_digit(n, i) != 9)
             {
-                
+                flag_compl++;
             }
+            else if (i == total_digits - 1 && return_digit(n, i) == 1)
+            {
+                flag_compl++;
+            }
+            else if (i > 0 && i < total_digits - 1 && return_digit(n, i) != 0)
+            {
+                flag_compl++;
+            }
+            else
+            {
+                break;
+            }
+
             // cout << return_digit(n, i) << "\n";
+        }
+        // cout << total_digits << "  ";
+        // cout << flag_compl << "  ";
+        // if (flag_compl == n)
+        if (flag_compl == total_digits)
+        {
+            cout << "YES" << "\n";
+        }
+        else
+        {
+            cout << "NO" << "\n";
         }
     }
 
