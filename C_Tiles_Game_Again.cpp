5b9e2a658aaf560e6a8a8da7951996339ed6aeb9
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-21 11:15:28 +0530

    aildsfjasdkfajsd

--- C_Tiles_Game_Again.cpp
+++ C_Tiles_Game_Again.cpp
@@ -17,26 +17,51 @@ int main()
         int space_ints[n];
         for (int j = 0; j < n; j++)
         {
-            cin>>space_ints[j];
+            cin >> space_ints[j];
         }
-        
+
         longest_streak = 0;
         temp_streak = 0;
-        for (int i = 0; i < n; i++)
+        if (n == 1)
         {
-            if (space_ints[i] == 0)
+            if (space_ints[0] == 0)
             {
-                temp_streak++;
-            }
-            else if (space_ints[i] == 1)
-            {
-                if (longest_streak < temp_streak)
-                {
-                    longest_streak = temp_streak;
-                }
-                temp_streak = 0;
+                longest_streak = 1;
             }
         }
+        else
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (space_ints[i] == 0)
+                {
+                    temp_streak++;
+                    if (i == n - 1 && longest_streak < temp_streak)
+                    {
+                        longest_streak = temp_streak;
+                        temp_streak = 0;
+                    } // this was added as a measure to encounter the edge case where the ending is 0 and the loop doesn't check the conditions again
+                }
+                // else if (space_ints[i] == 1)
+                // {
+                /*                 else if (longest_streak <= temp_streak) //< to <=
+                                {
+                                    longest_streak = temp_streak;
+                                    temp_streak = 0;
+                                } */
+                else if (space_ints[i] == 1)
+                {
+                    if (longest_streak <= temp_streak) //< to <=
+                    {
+                        longest_streak = temp_streak;
+                    }
+                    temp_streak = 0;
+                }
+
+                // }
+            }
+        }
+
         cout << longest_streak << "\n";
     }
 
