b149069fe8dd4d2e382cf739c986082b1b4bdd31
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-21 17:00:17 +0530

    aldfjasldkfj

--- --------------------A_Array_Balancing.cpp
+++ --------------------A_Array_Balancing.cpp
@@ -2,7 +2,9 @@
 #define ll long long
 
 using namespace std;
-
+// exploring all the possibilities of a solution
+// not greedy
+// but after doing all that if you choose one possibility (some best case for example) then that is 'greedy'
 int main()
 {
 
@@ -10,12 +12,14 @@ int main()
     cin.tie(NULL);
 
     int t,n;
+    ll sum=0;
     cin >> t;
     for (int i = 0; i < t; i++)
     {
         // we have two options |Ap - Ap+1|+|Bp-Bp+1|
         // or we have |Bp - Ap+1|+|Ap-Bp+1| on swapping the ap and bp review this problem in 14th video
         cin>>n;
+        sum=0;
         int a[n],b[n];
         for (int i = 0; i < n; i++)
         {
@@ -23,18 +27,22 @@ int main()
         }
         for (int j = 0; j < n; j++)
         {
-            cin>>a[j];
+            cin>>b[j];
         }
         for (int i = 0; i < n-1; i++)
         {
-            if (1)
+            if (abs(a[i]-a[i+1])+abs(b[i]-b[i+1])>abs(b[i]-a[i+1])+abs(a[i]-b[i+1]))
             {
-                /* code */
+                swap(a[i+1],b[i+1]);
             }
             
         }
         
-        
+        for (int i = 0; i < n-1; i++)
+        {
+            sum+=abs(a[i]-a[i+1])+abs(b[i]-b[i+1]);
+        }
+        cout<<sum<<"\n";
         
     }
 
--- At_Most_3_Judge_ver.cpp
+++ At_Most_3_Judge_ver.cpp
@@ -3,33 +3,46 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n,w;
-    cin>>n>>w;
+    int n, w;
+    cin >> n >> w;
     int weights[n];
-    int single_flag=0;
-    int double_flag=0;
-    int triple_flag=0;
+    int single_flag = 0;
+    int double_flag = 0;
+    int triple_flag = 0;
     for (int i = 0; i < n; i++)
     {
-        cin>>weights[i];
+        cin >> weights[i];
     }
     for (int j = 0; j < n; j++)
     {
-        if (weights[j]<w)
+        if (weights[j] < w)
         {
-            
+            single_flag++;
         }
-        
+        for (int k = j + 1; k < n; k++)
+        {
+            if (weights[j] + weights[k] < w)
+            {
+                double_flag++;
+            }
+            for (int l = k + 1; l < n; l++)
+            {
+                if (weights[j] + weights[k] + weights[l] < w)
+                {
+                    triple_flag++;
+                }//blindly incrementing does not work since we want distinct integers
+            }
+        }
     }
-    
-    
+    cout << single_flag << "\n";
+    cout << double_flag << "\n";
+    cout << triple_flag << "\n";
 
-    
-    
     return 0;
 }
