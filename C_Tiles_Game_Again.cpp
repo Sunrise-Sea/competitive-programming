8807e775cfdcf39236191dcbc1974ce5a1d27468
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-21 10:51:04 +0530

    alsdfjalksdfjalsdf

--- C_Tiles_Game_Again.cpp
+++ C_Tiles_Game_Again.cpp
@@ -3,24 +3,42 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t,n;
-    cin>>t;
+    int t, n, longest_streak = 0, temp_streak = 0;
+    cin >> t;
     for (int i = 0; i < t; i++)
     {
-        cin>>n;
+        cin >> n;
         int space_ints[n];
-        for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
         {
-            
+            cin>>space_ints[j];
         }
         
+        longest_streak = 0;
+        temp_streak = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (space_ints[i] == 0)
+            {
+                temp_streak++;
+            }
+            else if (space_ints[i] == 1)
+            {
+                if (longest_streak < temp_streak)
+                {
+                    longest_streak = temp_streak;
+                }
+                temp_streak = 0;
+            }
+        }
+        cout << longest_streak << "\n";
     }
-    
-    
+
     return 0;
 }
