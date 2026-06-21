1a33a75c6feb8157665df47d1d241c0e6d8eb406
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-21 16:33:41 +0530

    commit to the bit

---
+++ At_Most_3_Judge_ver.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,w;
+    cin>>n>>w;
+    int weights[n];
+    int single_flag=0;
+    int double_flag=0;
+    int triple_flag=0;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>weights[i];
+    }
+    for (int j = 0; j < n; j++)
+    {
+        if (weights[j]<w)
+        {
+            
+        }
+        
+    }
+    
+    
+
+    
+    
+    return 0;
+}
--- Same_Map_in_the_RPG_World.cpp
+++ Same_Map_in_the_RPG_World.cpp
@@ -18,6 +18,16 @@ int main()
         for (int j = 0; j < W; j++)
         {
             cin >> matrix_a[i][j];
+            if (matrix_a[i][j]=='#')
+            {
+                hash_a++;
+            }
+            else
+            {
+                dot_a++;
+            }
+            
+            
         }
     }
     for (int i = 0; i < H; i++)
@@ -25,8 +35,28 @@ int main()
         for (int j = 0; j < W; j++)
         {
             cin >> matrix_b[i][j];
+            if (matrix_b[i][j]=='#')
+            {
+                hash_b++;
+            }
+            else
+            {
+                dot_b++;
+            }
         }
     }
+    // we can move in any direction we want using carrying out the that one operation n times + some which will resemble the reverse of that original operation
+    // check to see if it's even true
+    if (hash_a==hash_b&&dot_a==dot_b)
+    {
+        cout<<"Yes"<<"\n";
+    }
+    else
+    {
+        cout<<"No"<<"\n";
+    }
+    
+    
 
     return 0;
 }
