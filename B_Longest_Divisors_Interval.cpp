27a74b20257342c71296abeb813620b6cb80962a
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-22 17:04:26 +0530

    kajsldkfjasldkf

---
+++ B_Longest_Divisors_Interval.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; i++)
+    {
+        // n - given number, [l,r] some intervale, i in this is -> N%i=0
+        // 990990 =N hence [9,11] hence N%9 = 0 N%10 =0 N%11
+    }
+    
+    
+    return 0;
+}
+// what is asked to you also opens up a lot of possibilities, 
+// like they said to print interval not a unique interval hence means there may or may not be another interval
--- Discord.cpp
+++ Discord.cpp
@@ -17,6 +17,21 @@ int main()
             cin >> photos[i][j];
         }
     }
+    int people_besides[n][n];
+    memset(people_besides, -1, sizeof(people_besides));
+    for (int i = 0; i < n; i++)
+    {
+        people_besides[i][0] = i + 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << people_besides[i][j] << " ";
+        }
+        cout << "\n";
+    }
 
     return 0;
 }
