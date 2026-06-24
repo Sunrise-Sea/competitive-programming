c682eda9d0c4776331b84b766711de9acd32bde3
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-24 10:32:19 +0530

    hqyo

--- B_Blank_Space.cpp
+++ B_Blank_Space.cpp
@@ -9,20 +9,45 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t, n, max_blank = 0;
+    int t, n, travel = 0, zero_check = 0, temp_max_blank = 0, maxblank = 0;
     cin >> t;
     while (t--)
     {
-        max_blank = 0;
+        temp_max_blank = 0;
+        maxblank = 0;
+        travel = 0;
+        zero_check = 0;
         cin >> n;
         bool dingus[n];
         for (int i = 0; i < n; i++)
         {
             cin >> dingus[i];
         }
-        for (int ind = 0; ind < n - 1; ind++)
+        /*         for (int ind = 0; ind < n - 1; ind++)
+                {
+                    if (dingus[ind] == dingus[ind + 1])
+                    {
+                        temp_max_blank++;
+                    }
+                    else
+                    {
+                        if (temp_max_blank > maxblank)
+                        {
+                            maxblank = temp_max_blank;
+                        }
+                        temp_max_blank = 0;
+                    }
+                } */
+        while (travel < n)
         {
+            if (dingus[travel]==0)
+            {
+                zero_check++;
+            }
+            
         }
+
+        cout << maxblank << "\n";
     }
 
     return 0;
