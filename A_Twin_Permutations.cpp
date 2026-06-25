9721981a6fe4e6e8c2a52f462c19035106fd988a
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-26 00:21:21 +0530

    problem statement reread

--- A_Twin_Permutations.cpp
+++ A_Twin_Permutations.cpp
@@ -3,18 +3,42 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t;
-    cin>>t;
-    while(t--)
+    int t, n;
+    cin >> t;
+    while (t--)
     {
-        
+        cin >> n;
+        int twin_perm[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> twin_perm[i];
+        }
+        /*         for (int j = 0; j < n-1; j++)
+                {
+                    if (twin_perm[j]<=twin_perm[j+1])
+                    {
+                        cout<<twin_perm[j]<<"\n";
+                    }
+                    else
+                    {
+
+                    }
+
+
+                } */
+        //    since all are distinct elements and 1<=ai<=n hence all 1 to n numbers included hence we can just take best case and do that
+        // sum can be all equal hence min +max --> n+1
+        for (int j = 0; j < n; j++)
+        {
+            cout << n + 1 - twin_perm[j] << " ";
+        }
     }
-    
-    
+
     return 0;
 }
