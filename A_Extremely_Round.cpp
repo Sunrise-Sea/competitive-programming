ef0594139548b5d5206e45010708f6da805cca38
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-26 10:54:13 +0530

    how did i make a test case pass in the first try dayum

--- A_Extremely_Round.cpp
+++ A_Extremely_Round.cpp
@@ -3,18 +3,34 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t,n;
-    cin>>t;
-    while(t--)
+    int t, n, i, flag = 0, numbers = 0;
+    cin >> t;
+    while (t--)
     {
-        
+        cin >> n;
+        i = 0, flag = 0, numbers = 0;
+        while (n / (int)pow(10, i) >= 10)
+        {
+            flag = 1;
+            numbers+=9;
+            i++;
+        }
+        if (!flag)
+        {
+            cout << n << "\n";
+        }
+        else
+        {
+            numbers+= n/(int)pow(10,i);//
+            cout << numbers << "\n";
+        }
     }
-    
-    
+
     return 0;
 }
