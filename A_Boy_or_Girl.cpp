e0bbb5e628bee7fa596465ebc8d5dc2c0379590d
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-26 17:04:40 +0530

    initial

---
+++ A_Boy_or_Girl.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string t;
+    int charac =0 ;
+    cin>>t;
+    
+    // if (t.length()%2==0)
+    // {
+    //     cout<<"CHAT WITH HER!"<<"\n"; 
+    // }
+    // else
+    // {
+    //     cout<<"IGNORE HIM!"<<"\n";
+    // }
+    for (int i = 0; i < t.length(); i++)
+    {
+        for (int j = i+1; j < t.length(); j++)
+        {
+            if (t[j] == t[i])
+            {
+                charac++;
+            }
+            
+        }
+        
+    }
+    
+    
+
+    
+    
+    return 0;
+}
