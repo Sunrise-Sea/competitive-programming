3e3a088156dc95c29c8fb70ae71468147be7807c
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-27 11:03:43 +0530

    try the first thing

---
+++ C_Prepend_and_Append.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t, n;
+    cin >> t;
+    string binary;
+    while (t--)
+    {
+        cin >> n >> binary;
+        while (true)
+        {
+            if ((binary[n - 1] == '0' || binary[0] == '1') && (binary[n - 1] == '1' || binary[0] == '0'))
+            {
+                binary.pop_back();
+                binary.erase(0, 0);
+            }
+            else
+            {
+                break;
+            }
+        }
+        cout<<binary<<"\n";
+    }
+
+    return 0;
+}
