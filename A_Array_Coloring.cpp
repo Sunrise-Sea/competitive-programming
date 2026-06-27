871c727e0a06c7dc2bf6efce6b51223e6bcad70a
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-27 22:59:32 +0530

    let's do this

--- A_Array_Coloring.cpp
+++ A_Array_Coloring.cpp
@@ -18,7 +18,16 @@ int main()
         int color[n];
         for (int i = 0; i < n; i++)
         {
+            cin >> color[i];
+            // if (color[i])
+            // {
+            //     /* code */
+            // }
+            // testing to see if this slick method works
+            even += (int)(color[i] % 2 == 0);
         }
+        cout<<even<<" "<<n-even<<"\n";//let's devise some condition solely using the number of evens and odds since all we care about is pairity
+        
     }
 
     return 0;
