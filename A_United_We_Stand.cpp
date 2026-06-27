582316436a947935d182a1e0a2aa1eb33153f750
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-27 19:21:29 +0530

    once again

--- A_United_We_Stand.cpp
+++ A_United_We_Stand.cpp
@@ -3,27 +3,53 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t,n,max;
-    cin>>t;
-    while(t--)
+    int t, n, max = 0, min = INT_MAX, maxes = 0;
+    cin >> t;
+    while (t--)
     {
         // we can just put all the biggest numbers (all the multiple instances of max numbers to c and the rest in b)
         // since any solution is accepted
-        cin>>n;
+        cin >> n;
+        max = 0, min = INT_MAX, maxes = 0;
         int unite_us[n];
         for (int i = 0; i < n; i++)
         {
-            cin>>unite_us[i];
+            cin >> unite_us[i];
+            max < unite_us[i] ? max = unite_us[i] : 0;
+            min > unite_us[i] ? min = unite_us[i] : 0;
         }
-        
-        
+        if (max == min)
+        {
+            cout << "-1";
+        }
+        else
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (unite_us[i] == max)
+                    maxes++;
+            }
+            cout<<n-maxes<<" "<<maxes<<"\n";
+            for (int i = 0; i < n; i++)
+            {
+                if (unite_us[i] != max)
+                    (cout << unite_us[i] << " ");
+            }
+            cout << "\n";
+            for (int i = 0; i < n; i++)
+            {
+                if (unite_us[i] == max)
+                    (cout << unite_us[i] << " ");
+            }
+        }
+        cout << "\n";
     }
-    
-    
+
     return 0;
 }
