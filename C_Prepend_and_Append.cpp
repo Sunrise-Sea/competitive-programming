2b9bb46c86515f66580430dece416045cbc216ac
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-27 12:29:11 +0530

    hell yeah

--- C_Prepend_and_Append.cpp
+++ C_Prepend_and_Append.cpp
@@ -15,13 +15,20 @@ int main()
     while (t--)
     {
         cin >> n >> binary;
-        if ((binary[n - 1] == '0' && binary[0] == '1') || (binary[n - 1] == '1' && binary[0] == '0'))
+        for (int i = 0; i < 2; i++)
         {
-            // binary.pop_back();
-            // binary.erase(0, 1);
+            if ((binary[n - 1] == '0' && binary[0] == '1') || (binary[n - 1] == '1' && binary[0] == '0'))
+            {
+                // cout << (int)binary.length() << "\n";
+                binary.pop_back();
+                binary.erase(0, 1);
+                n-=2;
+                // forgot to decrease n
+            }
         }
         // binary.erase(0, 1);//number of characters to remove after that index start
-        cout << (int)binary.length() << "\n";
+        // cout << (int)binary.length() << "\n";
+        cout << n << "\n";
     }
 
     return 0;
