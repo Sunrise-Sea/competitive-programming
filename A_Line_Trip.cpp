f81944cef4d43752b07b3656495407ec372fb8c0
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-27 15:56:46 +0530

    one thing

---
+++ A_Line_Trip.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        int gas_stations[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin>>gas_stations[i];
+            
+        }
+        
+    }
+    
+    
+    return 0;
+}
