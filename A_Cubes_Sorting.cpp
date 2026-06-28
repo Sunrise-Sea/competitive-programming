32062e80a1129e9d7e560ced8e064849b68e9c35
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-28 18:11:27 +0530

    chenage

--- A_Cubes_Sorting.cpp
+++ A_Cubes_Sorting.cpp
@@ -4,30 +4,49 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-// since n(n-1)/2 then that means array is fully reversed
-    int t,n;
-    cin>>t;
-    while(t--)
+    // if n(n-1)/2 operations then that means array is fully reversed
+    int t, n;
+    cin >> t;
+    while (t--)
     {
-        cin>>n;
-        vector<int> hello(n);   
+        cin >> n;
+        // vector<int> hello(n);
+        int hello[n], flag = 0;
         for (int i = 0; i < n; i++)
         {
-            cin>>hello[i];
+            cin >> hello[i];
         }
-        vector<int> temporary(n) ;
+        // vector<int> temporary(n) ;
+        int temporary[n];
         for (int i = 0; i < n; i++)
         {
-           temporary[i]=hello[i];
+            temporary[i] = hello[i];
         }
-        sort(temporary.rend(),temporary.rbegin());
-        
+        sort(temporary, temporary + n);
+        for (int i = 0; i < n; i++)
+        {
+            if (temporary[n - 1 - i] == hello[i])
+            {
+            flag++;
+            }
+            
+        }
+
+        // if (temporary == hello)
+        if (flag==n)
+        {
+            cout << "NO" << "\n";
+        }
+        else
+        {
+            cout << "YES" << "\n";
+        }
     }
-    
-    
+
     return 0;
 }
