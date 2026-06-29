2252c8b339f43eb4dab20a20026043edbe103ae9
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-29 15:05:09 +0530

    sus

---
+++ A_Ehab_Fails_to_Be_Thanos.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+// #define ll long long
+// #define Loop(i, a, b) for (int i = a; i < b; i++)
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+    // while(t--)
+    // {
+        
+    // }
+    int yolo[2*n];
+    for (int i = 0; i < 2*n; i++)
+    {
+        cin>>yolo[i];
+    }
+    // if there is no permutable way it can have half elements sum != the other half then all must be equal
+    for (int j = 0; j < 2*n-1; j++)
+    {
+        
+    }
+    
+    
+    
+    
+    return 0;
+}
--- A_GCD_game.cpp
+++ A_GCD_game.cpp
@@ -4,19 +4,19 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t,n;
-    cin>>t;
-    while(t--)
+    int t, n;
+    cin >> t;
+    while (t--)
     {
-        cin>>n;
-        
+        cin >> n; // logic because we have to choose a number and it's always going to be less than a-b because subtraction in gcd
+        cout << (int)(n / 2) << "\n";
     }
-    
-    
+
     return 0;
 }
