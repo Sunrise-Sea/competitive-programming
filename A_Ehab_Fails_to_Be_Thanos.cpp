6326ca86a262e4f63e2fa00a7c7244819e303638
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-06-29 16:17:49 +0530

    we are thanos

--- A_Ehab_Fails_to_Be_Thanos.cpp
+++ A_Ehab_Fails_to_Be_Thanos.cpp
@@ -9,7 +9,7 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n;
+    int n,flag =0;
     cin>>n;
     // while(t--)
     // {
@@ -21,10 +21,28 @@ int main(){
         cin>>yolo[i];
     }
     // if there is no permutable way it can have half elements sum != the other half then all must be equal
-    for (int j = 0; j < 2*n-1; j++)
+    for (int j = 0; j < (2*n)-1; j++)
     {
+        if (yolo[j]==yolo[j+1])
+        {
+            flag++;
+        }
+    }
+    if (flag==(2*n)-1)
+    {
+        cout<<-1<<"\n";
+    }
+    else
+    {
+        sort(yolo,yolo+2*n);//since we have to juggle/move around elements anyway why not just sort it and have the sum be OBVIOUSLY unequal if the elements are unequal themselves   
+        for (int i = 0; i < 2*n; i++)
+        {
+            cout<<yolo[i]<<" ";
+        }
         
     }
+    
+    
     
     
     
