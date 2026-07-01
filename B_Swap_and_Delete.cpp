1ccc954de095648b3b8db1589a4e13770853d73a
Sunrise-Sea <pratyush.solanki17april@gmail.com>
2026-07-01 13:06:21 +0530

    why can't i do it

--- B_Swap_and_Delete.cpp
+++ B_Swap_and_Delete.cpp
@@ -4,18 +4,52 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
+    // int t, n;
     int t;
-    cin>>t;
-    while(t--)
+    string st;
+    cin >> t;
+    while (t--)
     {
-        
+        int zeroes = 0, ones = 0;
+        int zeroes_after = 0, ones_after = 0;
+        cin >> st;
+        // vector<int> st(n);
+        // for (int i = 0; i < n; i++)
+        // {
+        //     cin >> st[i];
+        //     zeroes += (int)(st[i] == 0);
+        // }// it didn't ask for n
+        for (int i = 0; i < (int)st.length(); i++)
+        {
+            zeroes += (int)(st[i] == '0');
+        }
+        ones = (int)st.length() - zeroes;
+        if (zeroes == ones)
+        {
+            cout << 0 << "\n";
+        }
+        else
+        {
+            cout << abs((int)st.length() - (2 * zeroes)) << "\n";
+            if (ones > zeroes)
+            {
+                ones_after = ones - (abs((int)st.length() - (2 * zeroes)));
+            }
+            else
+            {
+                zeroes_after = zeroes - (abs((int)st.length() - (2 * zeroes)));
+            }
+        }
+        //    cout<<zeroes<<"\n";
+
+        // we need equal number of 1's and 0's to swap it to opposite places so that none equals the former
+        // since swapping does not have any costs
     }
-    
-    
+
     return 0;
 }
